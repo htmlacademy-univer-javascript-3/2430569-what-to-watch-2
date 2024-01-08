3aa23abf5e22bafea27252d35f5ce47d66e26d7d
Dgcim1 <gindra2001@gmail.com>
2024-01-08 11:09:36 +0300

    6.16. Контроль и ограничения (часть 2)

--- src/components/catalog.tsx
+++ src/components/catalog.tsx
@@ -1,22 +1,32 @@
 import {CatalogGenreList} from './catalog-genre-list.tsx';
 import {CatalogFilmList} from './catalog-film-list.tsx';
+import {useAppSelector} from '../store/hooks.ts';
+import {useCallback, useState} from 'react';
 
 interface Props {
   withoutGenres?: boolean;
   withoutShowMoreButton?: boolean;
 }
 
-const CatalogShowMoreButton = () => (
+const CatalogShowMoreButton = ({onClick} : {onClick: () => void}) => (
   <div className="catalog__more">
-    <button className="catalog__button" type="button">Show more</button>
+    <button className="catalog__button" type="button" onClick={onClick}>Show more</button>
   </div>
 );
 
-export const Catalog = ({withoutGenres = false, withoutShowMoreButton = false}: Props) => (
-  <section className="catalog">
-    <h2 className="catalog__title visually-hidden">Catalog</h2>
-    {!withoutGenres ? <CatalogGenreList/> : null}
-    <CatalogFilmList maxCountFilter={8}/>
-    {!withoutShowMoreButton ? <CatalogShowMoreButton/> : null}
-  </section>
-);
+export const Catalog = ({withoutGenres = false, withoutShowMoreButton = false}: Props) => {
+  const LIST_LENGTH_STEP = 8;
+  const stateFilms = useAppSelector((state) => state.films);
+  const [length, setLength] = useState(LIST_LENGTH_STEP);
+  const handleClick = useCallback(()=>{
+    setLength((prev) => prev + LIST_LENGTH_STEP);
+  },[]);
+  return (
+    <section className="catalog">
+      <h2 className="catalog__title visually-hidden">Catalog</h2>
+      {!withoutGenres ? <CatalogGenreList/> : null}
+      <CatalogFilmList maxCountFilter={length}/>
+      {!withoutShowMoreButton && stateFilms.length > length ? <CatalogShowMoreButton onClick={handleClick}/> : null}
+    </section>
+  );
+};
